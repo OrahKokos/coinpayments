b232d829a3533594d9d00564b6d0c10d6f0252e4
duper51 <ianotto24@gmail.com>
2017-12-31 13:37:06 -0700

    bump new fix to testing

--- src/ipn.js
+++ src/ipn.js
@@ -4,7 +4,7 @@ const
 
 module.exports = (function () {
 
-    function IPN ({merchantId, merchantSecret, reqBodyIndex='body'}) {
+    function IPN ({merchantId, merchantSecret}) {
         if(!merchantId || !merchantSecret) {
             throw "Merchant ID and Merchant Secret are needed";
         }
@@ -12,22 +12,20 @@ module.exports = (function () {
 
         let getPrivateHeadersIPN = function (parameters) {
             let signature, paramString;
+            if(typeof parameters === "object")
+                parameters = qs.stringify(parameters, null, null, { encodeURIComponent: null }).replace(/\s/g, '+');
 
-            if(typeof reqBodyIndex === "object") {
-                paramString = qs.stringify(parameters).replace(/%20/g, '+');
-            }
-
-            signature = crypto.createHmac('sha512', merchantSecret).update(paramString).digest('hex');
+            signature = crypto.createHmac('sha512', merchantSecret).update(parameters).digest('hex');
 
             return signature;
         };
 
         return (req,res,next) => {
-            if(!req.get('HMAC') || !req.body || !req.body.ipn_mode || req.body.ipn_mode !== 'hmac' || merchantId !== req.body.merchant) {
+            if(!req.get('HMAC') || !req.body || !req.body.ipn_mode || req.body.ipn_mode != 'hmac' || merchantId != req.body.merchant) {
                 return next("COINPAYMENTS_INVALID_REQUEST");
             }
-            hmac = getPrivateHeadersIPN(req[reqBodyIndex]);
-            if(hmac !== req.get('HMAC')) {
+            hmac = getPrivateHeadersIPN(req.body);
+            if(hmac != req.get('HMAC')) {
                 return next("COINPAYMENTS_INVALID_REQUEST");
             }
             res.end();
@@ -39,7 +37,7 @@ module.exports = (function () {
                 this.emit('ipn_pending', req.body);
                 return next();
             }
-            if(req.body.status === 100) {
+            if(req.body.status == 100) {
                 this.emit('ipn_complete', req.body);
                 return next();
             }
