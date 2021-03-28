36f03e9bcaaba597aac4a1b85c524c4cceabf038
OrahKokos <marko.paroski.ns@gmail.com>
2021-03-28 11:45:07 +0000

    Fix create trasaction type, missing field

--- src/types/response.ts
+++ src/types/response.ts
@@ -83,6 +83,7 @@ export interface CoinpaymentsCreateTransactionResponse {
   address: string
   confirms_needed: string
   timeout: number
+  checkout_url: string
   status_url: string
   qrcode_url: string
 }
