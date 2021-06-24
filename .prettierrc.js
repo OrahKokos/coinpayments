5c6bbfc46596333b9265abc9f574fdfd5a15356a
OrahKokos <orah@kokos.tech>
2021-06-24 09:21:28 +0000

    update dev deps + prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "arrowParens": "avoid",
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "bracketSpacing": true
+};
